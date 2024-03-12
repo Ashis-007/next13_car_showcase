ebdb8ae5954ccd7d7f9b7e0975a78d91f1e94a2f
Ashis-007 <ashisjc@gmail.com>
2024-03-12 11:18:37 +0530

    temp: add slackbot script

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,26 @@ export const metadata = {
   description: "Discover world's best car showcase application",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang='en'>
-      <body className='relative'>
+    <html lang="en">
+      <head>
+        <script
+          defer
+          src="https://slack-support-stage.ringover-crm.xyz/js-livechat-iframe/main.js"
+          userCdnId="0m13psBX7J5Av4qEOaJBD"
+        ></script>
+        <link
+          rel="stylesheet"
+          href="https://slack-support-stage.ringover-crm.xyz/css-livechat-iframe/main.css"
+        />
+      </head>
+
+      <body className="relative">
         <NavBar />
         {children}
         <Footer />
